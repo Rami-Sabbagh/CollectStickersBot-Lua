8b5b8dfc1743ca35225c6793938438456120abb3
Rami Sabbagh <ramilego4game@gmail.com>
2020-05-20 21:30:53 +0300

    Move the help commands into the basic module

--- modules/basic.lua
+++ modules/basic.lua
@@ -1,7 +1,31 @@
 --Basic module
 
+local function sendHelp(chat)
+    chat:sendMessage(table.concat({
+        "I can help you clone stickers into your own collection packs",
+        "",
+        "Just send me the sticker and I'll take care of creating the stickers pack and reuploading the sticker.",
+        "",
+        "Then you can manage the stickers packs using the @Stickers bot."
+    }, "\n"))
+end
+
 --The module's commands array
 local commands = {}
+
+--------------------------------[[ /start command ]]--------------------------------
+
+function commands.start(message)
+    if not message then return end
+    sendHelp(message.chat)
+end
+
+--------------------------------[[ /help command ]]--------------------------------
+
+function commands.help(message)
+    if not message then return "What can this bot do?" end
+    sendHelp(message.chat)
+end
 
 --------------------------------[[ /ping command ]]--------------------------------
 
--- modules/stickers.lua
+++ modules/stickers.lua
@@ -47,32 +47,8 @@ local function addSticker(response, sticker, stickerData)
     end
 end
 
-local function sendHelp(chat)
-    chat:sendMessage(table.concat({
-        "I can help you clone stickers into your own collection packs",
-        "",
-        "Just send me the sticker and I'll take care of creating the stickers pack and reuploading the sticker.",
-        "",
-        "Then you can manage the stickers packs using the @Stickers bot."
-    }, "\n"))
-end
-
 --The module's commands array
 local commands = {}
-
---------------------------------[[ /start command ]]--------------------------------
-
-function commands.start(message)
-    if not message then return end
-    sendHelp(message.chat)
-end
-
---------------------------------[[ /help command ]]--------------------------------
-
-function commands.help(message)
-    if not message then return "What can this bot do?" end
-    sendHelp(message.chat)
-end
 
 --------------------------------[[ Raw updates handler ]]--------------------------------
 
